42c172a76d419178102ed1b400fdb0932283adc0
kamarajugadda <salini2579@gmail.com>
2025-03-28 10:44:00 +0530

    test

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = var.s3_bucket_name     # Your S3 bucket name
+    bucket         = "salini-nginx"     # Your S3 bucket name
     key            = "infra/terraform.tfstate"       # Path to state file
-    region         = var.aws_region              # AWS region
+    region         = "ap-south-1"              # AWS region
     dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
     encrypt        = true                        
   }
