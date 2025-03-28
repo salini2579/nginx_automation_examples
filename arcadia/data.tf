a3c68e26a8b4416ae5a158543d873d3f00eea1ee
kamarajugadda <salini2579@gmail.com>
2025-03-28 10:21:51 +0530

    Deploy test

--- arcadia/data.tf
+++ arcadia/data.tf
@@ -2,9 +2,9 @@
 data "terraform_remote_state" "infra" {
   backend = "s3"
   config = {
-    bucket =  "salini-ngx"       # Your S3 bucket name
+    bucket =  "salini-nginx"       # Your S3 bucket name
     key    = "infra/terraform.tfstate"       # Path to infra's state file
-    region = "us-east-1"                     # AWS region
+    region = "ap-south-1"                     # AWS region
   }
 }
 
@@ -12,18 +12,18 @@ data "terraform_remote_state" "infra" {
 data "terraform_remote_state" "nap" {
   backend = "s3"
   config = {
-    bucket =  "salini-ngx"       # Your S3 bucket name
+    bucket =  "salini-nginx"       # Your S3 bucket name
     key    = "nap/terraform.tfstate"         # Path to NAP state file
-    region = "us-east-1"                     # AWS region
+    region = "ap-south-1"                     # AWS region
   }
 }
 
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket =  "salini-ngx"       # Your S3 bucket name
+    bucket =  "salini-nginx"       # Your S3 bucket name
     key    = "eks-cluster/terraform.tfstate"  # Path to EKS state file
-    region = "us-east-1"                     # AWS region
+    region = "ap-south-1"                     # AWS region
   }
 }
 
