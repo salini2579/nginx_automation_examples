5398fc13894c090da502c2f2228381d1cd459ee5
kamarajugadda <salini2579@gmail.com>
2025-04-01 14:16:59 +0530

    storage test storage.tf update

--- azure/blob/storage.tf
+++ azure/blob/storage.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "tfstate"
-  location = "southeastasia"
+  name     = "tfstate-rg"
+  location = var.azure_region
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "tfstate-storage-account"
+  name                     = "tfstate-storage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name                  = "tfstate"
+  name                  = "tfstate-contqiner"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "blob"
 }
