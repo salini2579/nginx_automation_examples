05dd416a12db01f5fc776270664270a74cf84563
kamarajugadda <salini2579@gmail.com>
2025-04-01 22:21:22 +0530

    Infra deploy

--- azure/infra/variables.tf
+++ azure/infra/variables.tf
@@ -3,34 +3,6 @@ variable "azure_credentials" {
   type = string
 }
 
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-  sensitive   = true
-  default = jsondecode(var.azure_credentials)["subscriptionId"]
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-  sensitive   = true
-  default = jsondecode(var.azure_credentials)["tenantId"]
-}
-
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-  sensitive   = true
-  default = jsondecode(var.azure_credentials)["clientId"]
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type      = string
-  sensitive = true
-  default = jsondecode(var.azure_credentials)["clientSecret"]
-}
-
 variable "azure_region" {
   description = "Azure region for resources"
   type        = string
