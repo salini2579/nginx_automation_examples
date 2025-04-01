593bf6cd4224e118868b75fdf1584ae3628df32c
kamarajugadda <salini2579@gmail.com>
2025-04-01 14:18:56 +0530

    storage test storage.tf update underscore

--- azure/blob/storage.tf
+++ azure/blob/storage.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "tfstate-storage"
+  name                     = "tfstate_storage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name                  = "tfstate-contqiner"
+  name                  = "tfstate_container"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "blob"
 }
