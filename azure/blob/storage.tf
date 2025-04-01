d8fc56c528fee7f208327798b5298009331d7bda
kamarajugadda <salini2579@gmail.com>
2025-04-01 14:21:48 +0530

    storage test storage.tf update

--- azure/blob/storage.tf
+++ azure/blob/storage.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "tfstate_storage"
+  name                     = "tfstatestorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
