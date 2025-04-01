e40e5b9777d8b189c9eb3bd70b11be5703fb672f
kamarajugadda <salini2579@gmail.com>
2025-04-01 20:11:23 +0530

    deploy blob new

--- azure/blob/main.tf
+++ azure/blob/main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
   features {}
-#  subscription_id = jsondecode(var.azure_credentials)["subscriptionId"]
-#  client_id       = jsondecode(var.azure_credentials)["clientId"]
-#  client_secret   = jsondecode(var.azure_credentials)["clientSecret"]
-#  tenant_id       = jsondecode(var.azure_credentials)["tenantId"]
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
+  subscription_id = jsondecode(var.azure_credentials)["subscriptionId"]
+  client_id       = jsondecode(var.azure_credentials)["clientId"]
+  client_secret   = jsondecode(var.azure_credentials)["clientSecret"]
+  tenant_id       = jsondecode(var.azure_credentials)["tenantId"]
+#  subscription_id = var.subscription_id
+#  client_id = var.client_id
+#  client_secret = var.client_secret
+#  tenant_id = var.tenant_id
 }
--- azure/blob/variables.tf
+++ azure/blob/variables.tf
@@ -1,36 +1,36 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  type      = string
-  sensitive = true
-}
+#variable "subscription_id" {
+#  description = "Azure Subscription ID"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "tenant_id" {
+#  description = "Azure Tenant ID"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "client_id" {
+#  description = "Azure Client ID"
+#  type        = string
+#  sensitive   = true
+#}
+#
+#variable "client_secret" {
+#  description = "Azure Client Secret"
+#  type      = string
+#  sensitive = true
+#}
 
 variable "azure_region" {
   description = "Azure region for resources"
   type        = string
 }
 
-#variable "azure_credentials" {
-##  description = "Azure credentials as a JSON string"
-#  type = string
-#}
+variable "azure_credentials" {
+  description = "Azure credentials as a JSON string"
+  type = string
+}
 
 #variable "subscription_id" {
 #  description = "The Azure subscription ID"
