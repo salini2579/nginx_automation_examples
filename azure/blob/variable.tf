1122241583e1a44e1efa4d32b54095598bbe284b
kamarajugadda <salini2579@gmail.com>
2025-04-01 14:30:46 +0530

    test

--- azure/blob/variable.tf
+++ azure/blob/variable.tf
@@ -1,29 +1,24 @@
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
-  sensitive   = true
-  default     = null
 }
 
 variable "tenant_id" {
   description = "Azure Tenant ID"
   type        = string
   sensitive   = true
-  default     = null
 }
 
 variable "client_id" {
   description = "Azure Client ID"
   type        = string
   sensitive   = true
-  default     = null
 }
 
 variable "client_secret" {
   description = "Azure Client Secret"
   type      = string
   sensitive = true
-  default   = null
 }
 
 variable "azure_region" {
