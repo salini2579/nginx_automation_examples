e4c430e039ab9b88250c71bf5cf1db602815c75a
kamarajugadda <salini2579@gmail.com>
2025-04-02 12:58:39 +0530

    Blob, infra and state file test

--- azure/blob/storage.tf
+++ azure/blob/storage.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.azure_region
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -13,6 +13,6 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = var.container_name
-  storage_account_name  = var.storage_account_name
+  storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "blob"
 }
