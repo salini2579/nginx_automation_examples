e5d658ed456259f07623c872416c13dbc4730f92
kamarajugadda <salini2579@gmail.com>
2025-04-04 11:43:43 +0530

    AKS test

--- azure/aks/outputs.tf
+++ azure/aks/outputs.tf
@@ -1,5 +1,5 @@
 output "kubeconfig" {
-  value = data.azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
 output "cluster_name" {
@@ -8,5 +8,9 @@ output "cluster_name" {
 }
 output "cluster_id" {
   value = azurerm_kubernetes_cluster.aks.id
+  sensitive = true
+}
+output "aks_host" {
+  value = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
   sensitive = true
 }
