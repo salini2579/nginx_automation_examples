ba8cbf2e2930d5f923e61fa7a43a3f894e8aea67
kamarajugadda <salini2579@gmail.com>
2025-04-07 17:47:57 +0530

    nap

--- azure/nap/outputs.tf
+++ azure/nap/outputs.tf
@@ -1,9 +1,9 @@
-#output "external_name" {
-#    value = try(data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname, null)
-#}
-#output "external_port" {
-#    value = try(data.kubernetes_service_v1.nginx-service.spec.0.port.0.port, null)
-#}
+output "external_name" {
+    value = try(data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname, null)
+}
+output "external_port" {
+    value = try(data.kubernetes_service_v1.nginx-service.spec.0.port.0.port, null)
+}
 output "origin_source" {
     value = "nap"
 }
