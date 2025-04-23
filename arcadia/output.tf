abef51a75fc494bc04bccc14b0fec7962a832629
kamarajugadda <salini2579@gmail.com>
2025-04-23 11:21:17 +0530

    New test

--- arcadia/output.tf
+++
@@ -1,5 +0,0 @@
-output "external_name" {
-  description = "The external hostname of NGINX Ingress from NAP"
-  value       = data.terraform_remote_state.nap.outputs.external_name
-
-}
--- infra/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    key            = "infra/terraform.tfstate"       # Path to state file
-    dynamodb_table = "terraform-lock-table"          # DynamoDB table for state locking
-    encrypt        = true                        
-  }
-}
