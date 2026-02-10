f1637859eeea33815667ed0a73958fb13b4ac6c8
kamarajugadda <salini2579@gmail.com>
2026-02-10 20:37:46 +0530

    AZ Deploy

--- azure/nap/prometheus.tf
+++ azure/nap/prometheus.tf
@@ -5,6 +5,4 @@ resource "helm_release" "prometheus" {
     #version = "27.3.0"
     namespace = kubernetes_namespace_v1.monitoring.metadata[0].name
     values = [file("./charts/prometheus/values.yaml")]
-
-
 }
